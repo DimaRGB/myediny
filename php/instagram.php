8d33f014f60b65011b224394fb5911da5f9cdc2d
Skavulyak Dmytro <dima-rgb@Skavulyaks-MacBook-Air.local>
2013-11-04 16:46:25 +0200

    fixed bug with kerilica in uri

--- php/instagram.php
+++ php/instagram.php
@@ -6,7 +6,7 @@ header('Content-Type:application/json');
 echo getJsonInstagram();
 
 function getJsonInstagram() {
-	$url = 'http://instagram.com/tags/мыедины/feed/recent.rss';
+	$url = 'http://instagram.com/tags/%D0%BC%D1%8B%D0%B5%D0%B4%D0%B8%D0%BD%D1%8B/feed/recent.rss';
 	return xmlToJson($url);
 }
 
